3bef9d91fab06ac2d70a8bc96aef3be1f544f5b4
Phil Keeling <philip.keeling@outlook.com>
2016-01-13 22:21:21 +0000

    Set up unit testing.

---
+++ IniDotNet.Tests/Parsers/DefaultFileParserTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using IniDotNet.Parsers;
+using Xunit;
+
+namespace IniDotNet.Tests.Parsers
+{
+    public class DefaultFileParserTests
+    {
+        [Fact]
+        public void ReturnsNothingWhenProvidedEmptyString()
+        {
+            IFileParser parser = new DefaultFileParser();
+            var sections = parser.Parse(new StringReader(string.Empty));
+
+            Assert.False(sections.Any());
+        }
+    }
+}
