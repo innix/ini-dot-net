503418e6397664963feaf21dfe5a2b434cd1cfa6
Phil Keeling <phil.keeling@vdomain.co.uk>
2016-06-06 17:22:31 +0100

    Added DnsEndPoint converter with tests.

---
+++ IniDotNet.Tests/Converters/DnsEndPointConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using IniDotNet.Converters;
+using Xunit;
+
+namespace IniDotNet.Tests.Converters
+{
+    public class DnsEndPointConverterTests
+    {
+        [Theory]
+        [InlineData("127.0.0.1", 1234)]
+        [InlineData("www.google.co.uk", 80)]
+        [InlineData("localhost", 443)]
+        public void ConversionSucceeds(string host, int port)
+        {
+            // Arrange.
+            IConverter converter = new DnsEndPointConverter();
+
+            // Act.
+            object result;
+            bool converted = converter.TryConvertTo(typeof(DnsEndPoint), $"{host}:{port}", out result);
+            DnsEndPoint ep = result as DnsEndPoint;
+
+            // Assert.
+            Assert.True(converted);
+            Assert.NotNull(ep);
+            Assert.Equal(host, ep.Host);
+            Assert.Equal(port, ep.Port);
+        }
+    }
+}
---
+++ IniDotNet/Converters/DnsEndPointConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace IniDotNet.Converters
+{
+    public sealed class DnsEndPointConverter : IConverter
+    {
+        public bool TryConvertTo(Type type, string stringValue, out object converted)
+        {
+            if (type != typeof(DnsEndPoint))
+            {
+                converted = null;
+                return false;
+            }
+
+            int idx = stringValue.IndexOf(':');
+            if (idx == -1)
+            {
+                converted = null;
+                return false;
+            }
+
+            string host = stringValue.Substring(0, idx);
+
+            int port;
+            if (!int.TryParse(stringValue.Substring(idx + 1), out port))
+            {
+                converted = null;
+                return false;
+            }
+
+            try
+            {
+                converted = new DnsEndPoint(host, port);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                converted = null;
+                return false;
+            }
+        }
+    }
+}
