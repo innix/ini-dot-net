fc6682a9d0e578ca474d3fd7fc731ee713c1f3d7
Phil Keeling <phil.keeling@vdomain.co.uk>
2016-06-07 11:55:46 +0100

    Disable auto TimeSpan deserialisation.

--- IniDotNet/Converters/StandardConverter.cs
+++ IniDotNet/Converters/StandardConverter.cs
@@ -7,6 +7,15 @@ namespace IniDotNet.Converters
     {
         public bool TryConvertTo(Type type, string stringValue, out object converted)
         {
+            // .NET has a built-in converter for string-to-TimeSpan, but it is very lenient on which
+            // formats it will accept and will almost never parse it correctly. So we will disable
+            // built-in TimeSpan conversion and allow users to write their own converters.
+            if (type == typeof(TimeSpan))
+            {
+                converted = null;
+                return false;
+            }
+
             TypeConverter tc = TypeDescriptor.GetConverter(type);
 
             if (tc.CanConvertFrom(typeof(string)))
